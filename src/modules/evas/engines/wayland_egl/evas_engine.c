20f9effcbebfac92e3a54f4eafcdcd297795baa6
Cedric BAIL <cedric@osg.samsung.com>
2017-07-25 15:30:29 -0700

    evas: make the info size a parameter of the function to make it safer to roll in.

--- src/modules/evas/engines/wayland_egl/evas_engine.c
+++ src/modules/evas/engines/wayland_egl/evas_engine.c
@@ -1390,7 +1390,7 @@ module_open(Evas_Module *em)
    if (!em) return 0;
 
    /* get whatever engine module we inherit from */
-   if (!_evas_module_engine_inherit(&pfunc, "gl_generic")) return 0;
+   if (!_evas_module_engine_inherit(&pfunc, "gl_generic", sizeof (Evas_Engine_Info_Wayland))) return 0;
 
    /* setup logging domain */
    if (_evas_engine_wl_egl_log_dom < 0)
@@ -1421,8 +1421,6 @@ module_open(Evas_Module *em)
    ORD(image_native_set);
    ORD(image_native_init);
    ORD(image_native_shutdown);
-
-   func.info_size = sizeof (Evas_Engine_Info_Wayland);
 
    symbols();
 
